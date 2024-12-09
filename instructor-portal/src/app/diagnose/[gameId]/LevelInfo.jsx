1c36c465d0347c2b8cbe2e42a9ad30c7ed19ff2c
EyadTamer999 <eyadhassan2002@gmail.com>
2024-12-09 16:32:41 +0200

    feat: update LevelInfo component to display dynamic extraInfo prop instead of static text

--- instructor-portal/src/app/diagnose/[gameId]/LevelInfo.jsx
+++ instructor-portal/src/app/diagnose/[gameId]/LevelInfo.jsx
@@ -2,10 +2,8 @@ const LevelInfo = ({ extraInfo }) => (
   <>
     <div className="bg-white p-4 rounded-lg shadow-md">
       <h1 className="text-xl font-bold text-center">Extra Info/Patient Data</h1>
-      <p className="bg-gray-50 p-4 rounded-md overflow-y-auto max-h-40">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <p className="bg-gray-50 p-4 rounded-md overflow-y-auto max-h-40">
+        {extraInfo}
       </p>
     </div>
   </>
